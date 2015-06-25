34a9c4509aa8c305e674732919e3d640dc7a04e8
Wellingto Junior <junior.mac91@gmail.com>
2015-06-25 10:48:33 -0300

    Inserido o logo nas paginas, e a codificação das mensagens

--- enviarEmail.php
+++ enviarEmail.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-<meta charset="utf-8">
+<meta charset="UTF-8">
 <title>MJ Transportes e Logística</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta name="description" content="" />
@@ -12,9 +12,6 @@
 <link href="css/jcarousel.css" rel="stylesheet" />
 <link href="css/flexslider.css" rel="stylesheet" />
 <link href="css/style.css" rel="stylesheet" />
-
-
-<!-- Theme skin -->
 <link href="skins/default.css" rel="stylesheet" />
 
 </head>
@@ -25,29 +22,32 @@
 
 <?php 
 
-$nome = filter_var($_POST['nome'],FILTER_SANITIZE_STRIPPED);
+$nome = filter_var($_POST['nome'],FILTER_SANITIZE_STRING);
 $email = filter_var($_POST['email'],FILTER_SANITIZE_EMAIL);
-$assunto = filter_var($_POST['assunto'], FILTER_SANITIZE_EMAIL);
+$assunto = filter_var($_POST['assunto'], FILTER_SANITIZE_STRING);
+$telefone = filter_var($_POST['telefone'],FILTER_SANITIZE_STRING);
+
 $mensagem = filter_var($_POST['mensagem'], FILTER_SANITIZE_STRING);
+
 
 /*$email = filter_var_array($_POST,array('email' => FILTER_SANITIZE_EMAIL,
 									   'email' => FILTER_VALIDATE_EMAIL));*/
 $to = 'mj.sac@mjtransportes.com';
 
-/*
-$headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-$headers .= "From: $emaildominio\r\n";
-$headers .= "Return-Path: $emaildominio\r\n";
-*/
 
-$EmailMessage = 'Mensagem: ' . $mensagem . '<br><br>' . ' De: '. $nome . '<br>' . 'E-mail: ' . $email;
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers  = 'Content-type: text/html; charset=utf-8' . "\r\n"; 
+
+$EmailMessage = 'Mensagem: ' . $mensagem .
+	   '<br>' . ' De: '. $nome . 
+       '<br>' . 'Telefone: ' . $telefone . 
+       '<br>' . 'E-mail: ' . $email;
 
 if (mail($to, $assunto, $EmailMessage, $headers)){
-	echo 'Olá ' .$nome . ' Obrigado pelo seu contato' . '<br>' .
-		 'Sua mensagem foi enviada com sucesso';
+	echo 'Olá ', $nome , ' Obrigado pelo seu contato <br>
+		  Sua mensagem foi enviada com sucesso <br><br>';
 }else {
-	echo 'Desculpe, sua mensagem não foi enviada, tente novamente mais tarde!' . '<br><br>';
+	echo 'Desculpe, sua mensagem não foi enviada, tente novamente mais tarde! <br><br>';
 }
 	
 ?>
--- index.php
+++ index.php
@@ -14,7 +14,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>                
-                    <a class="navbar-brand" href="index.php"><img alt="Logo" src="img/logo.png"></a>
+                    <a class="navbar-brand" href="index.php"><img alt="Logo" src="img/logo.png" title="Home"></a>
                 </div>
                 <div class="navbar-collapse collapse ">
                     <ul class="nav navbar-nav">
